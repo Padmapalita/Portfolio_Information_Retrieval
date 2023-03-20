ee0a32bed3c276c58e96480a21685090c894f595
Padmapalita <padmapalita@abovebelowaround.com>
2023-03-20 13:43:20 +0000

    working

--- src/search.py
+++ src/search.py
@@ -4,18 +4,28 @@ import pickle
 
 class Search:
     def __init__(self,):
-        #self.bm25_df = pd.read_pickle("../../Files/Local_pickles/BM25_in_one_index.pkl")  
+        self.bm25_df = pd.read_pickle("../Files/Local_pickles/BM25_in_one_index.pkl")  
         print("un-pickled")
 
     def return_bm25_index():
-        bm25_df = pd.read_pickle("../../Files/Local_pickles/BM25_in_one_index.pkl")  
+        bm25_df = pd.read_pickle("../Files/Local_pickles/BM25_in_one_index.pkl")  
         return bm25_df
 
-    def retrieve_ranking(self, query, bm25_df ):
+    def retrieve_ranking(self, query ):
+        """
+        returns the BM25 results as a list of tuples (df.index,value, BM25_score)
+        """
         q_terms = query.split(' ')
-        q_terms_only = bm25_df[q_terms]
+        q_terms_only = self.bm25_df[q_terms]
         score_q_d = q_terms_only.sum(axis=1)
-        return sorted(zip(bm25_df.index.values, score_q_d.values),
+        return sorted(zip(self.bm25_df.index.values, score_q_d.values),
                         key = lambda tup:tup[1],
                         reverse=True)
+    
+    def lookup_titles(list):
+        """
+        This version should return human readable results 
+        """
+        ### TODO: Need to have the retrieve ranking return index.values AND episode information
+        # return human_readable 
 
