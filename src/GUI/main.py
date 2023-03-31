450122f6a72f6344d8934e2dc65445baf9d2bc6c
Padmapalita <padmapalita@abovebelowaround.com>
2023-03-31 11:08:58 +0100

    GUI_3_first_commit

--- src/GUI/main.py
+++ src/GUI/main.py
@@ -5,26 +5,31 @@
 # import os
 # os.system("pip list")
 import PySimpleGUI as sg
-
+sg.theme('DarkGrey2') 
 # layout
 sg.set_options(font='Default 18', keep_on_top=True)
 layout = [
-        [sg.Text("Hello world")],
-        [sg.Text("Hello world")],
-        [sg.Button("ok")]
-
-
+    [sg.Text('Please enter your Name, Age, Phone')],
+    [sg.Text('Name', size =(15, 1)), sg.InputText()],
+    [sg.Text('Age', size =(15, 1)), sg.InputText()],
+    [sg.Text('Phone', size =(15, 1)), sg.InputText()],
+    [sg.Submit(), sg.Cancel()]
 ]
 
 
 # window
 
-window = sg.window("Title", layout)
+window = sg.Window("Title", layout)
 
 
 # event handlers
-event, value = window.read()
+event, values = window.read()
 
+# The input data looks like a simple list 
+# when automatic numbered
+print(event, values[0], values[1], values[2])
 
 # close
 window.close()
+
+  
